084fd723b6bf76688649f30ad06dcc12e0d4a0b1
Kathir Vel <sid@kathirvel.com>
2014-02-02 12:30:59 +0000

    0.1.8

--- app/code/community/Optimiseweb/MobileDetect/Helper/Data.php
+++ app/code/community/Optimiseweb/MobileDetect/Helper/Data.php
@@ -5,7 +5,7 @@
  *
  * @package     Optimiseweb_MobileDetect
  * @author      Kathir Vel (sid@optimiseweb.co.uk)
- * @copyright   Copyright (c) 2013 Optimise Web Limited
+ * @copyright   Copyright (c) 2014 Optimise Web
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 require_once Mage::getModuleDir('', 'Optimiseweb_MobileDetect') . DS . 'Library' . DS . 'Mobile_Detect.php';
